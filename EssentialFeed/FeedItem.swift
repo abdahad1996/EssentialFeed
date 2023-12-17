7172efa80dae7876b77f4d61d4c8e31e63fc6a54
abdul ahad <a.ahad@geomobile.de>
2023-12-17 05:10:30 +0100

    create feeditem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Abdul Ahad on 17.12.23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let Id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: String
+}

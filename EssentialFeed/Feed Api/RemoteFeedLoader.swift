cb3b904376f663f0323f06171ced44fd375adff8
abdul ahad <a.ahad@geomobile.de>
2023-12-18 22:30:08 +0100

    remove testable import and make remotefeedloader types public

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+public protocol HttpClient{
+    func get(from url:URL)
+    
+}
+
+public class RemoteFeedLoader{
+    
+    private let url:URL
+    private let client:HttpClient
+    
+    public init(url: URL, client: HttpClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(){
+        client.get(from: url)
+    }
+}

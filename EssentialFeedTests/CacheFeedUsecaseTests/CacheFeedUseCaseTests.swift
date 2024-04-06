2b09ec738725676ed7ce0e23b7fa379fe65c45e7
abdul ahad <abdul_ahad1996@live.com>
2024-04-06 03:48:08 +0200

    Save command requests new cache insertion on successful deletion

--- EssentialFeedTests/CacheFeedUsecaseTests/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/CacheFeedUsecaseTests/CacheFeedUseCaseTests.swift
@@ -10,26 +10,47 @@ import XCTest
 import EssentialFeed
 
 class FeedStore{
+    typealias deleteCompletion = (Error?) -> Void
    var deleteCacheFeedCallCount = 0
-    var insertCacheFeedCallCount = 0
+  var insertCacheFeedCallCount = 0
     
-    func deleteCacheFeed(){
+    var deletionCompletion = [deleteCompletion]()
+    
+    func deleteCacheFeed(completion:@escaping (Error?) -> Void){
         deleteCacheFeedCallCount += 1
+        deletionCompletion.append(completion)
+    }
+    
+    func completeDeletion(with error:Error,at index:Int = 0){
+        deletionCompletion[index](error)
+    }
+    
+    func completeDeletionSuccessFully(at index:Int = 0){
+        deletionCompletion[index](nil)
+
+    }
+    
+    func insert(_ items:[FeedItem]){
+        insertCacheFeedCallCount += 1
     }
 }
 
 class LocalFeedStore{
-    let feedStore:FeedStore
-    init(feedStore: FeedStore) {
-        self.feedStore = feedStore
-    }
-    func save(items:[FeedItem]){
-        feedStore.deleteCacheFeed()
+    let store:FeedStore
+    init(store: FeedStore) {
+        self.store = store
     }
     
-    func completeDeletion(with error:Error,at index:Int = 0){
-        
+    func save(items:[FeedItem]){
+        store.deleteCacheFeed {[unowned self] error in
+            if error == nil{
+                self.store.insert(items)
+            }
+        }
     }
+    
+     
+    
 }
 class CacheFeedUseCaseTests:XCTestCase{
     func test_init_doesNotDeleteCacheUponCreation(){
@@ -52,18 +73,31 @@ class CacheFeedUseCaseTests:XCTestCase{
         let items = [uniqueItem()]
         let deletionError = anyError()
         
-        sut.save(items: [uniqueItem()])
+        sut.save(items: items)
         
-        sut.completeDeletion(with: deletionError)
+        store.completeDeletion(with: deletionError)
 
         XCTAssertEqual(store.insertCacheFeedCallCount, 0)
-
         
+    }
+    
+    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion(){
+        let (store,sut) = makeSut()
+        let items = [uniqueItem()]
+       
+        sut.save(items:items)
+        store.completeDeletionSuccessFully()
+        
+        
+        
+        sut.save(items: [uniqueItem()])
+        XCTAssertEqual(store.insertCacheFeedCallCount, 1)
+
     }
     
     func makeSut(file:StaticString = #file, line:UInt = #line) -> (FeedStore,LocalFeedStore){
         let store = FeedStore()
-        let localFeedStore = LocalFeedStore(feedStore: store)
+        let localFeedStore = LocalFeedStore(store: store)
         trackForMemoryLeaks(store,file: file,line: line)
         trackForMemoryLeaks(localFeedStore,file: file,line: line)
         return (store,localFeedStore)

b9871d31aab892fe2310e6ad437aec17d5909e4f
abdul ahad <abdul_ahad1996@live.com>
2024-05-02 03:08:36 +0200

    Add ManagedCache and ManagedFeedImage model representations

--- EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -8,7 +8,23 @@
 import Foundation
 import XCTest
 import EssentialFeed
+import CoreData
 
+private class ManagedCache:NSManagedObject {
+    @NSManaged var timeStamp:Date
+    @NSManaged var feed:NSOrderedSet
+    
+}
+
+private class ManagedFeedImage:NSManagedObject {
+    @NSManaged var id:UUID
+    @NSManaged var imageDescription:String?
+    @NSManaged var location:String?
+    @NSManaged var url:URL
+    @NSManaged var cache:ManagedCache
+
+    
+}
 class CoreDataFeedStore:FeedStore{
     func deleteCacheFeed(completion: @escaping deleteCompletion) {
          

1f644ff651dc539ac02be6c0507c9dd68951a9db
abdul ahad <abdul_ahad1996@live.com>
2024-05-27 01:39:26 +0200

    Extract LoaderSpy to a separate file

---
+++ EssentialFeediOSTests/Helpers/FeedViewControllerTests+Assertions.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewController+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by macbook abdul on 27/05/2024.
+//
+
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+

a38e844079d3e128108c97541da04a112770cdf5
abdul ahad <abdul_ahad1996@live.com>
2024-05-27 01:27:33 +0200

    Extract UIRefreshControl test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -507,15 +507,7 @@ private extension FeedImageCell{
     }
     
 }
-private extension UIRefreshControl {
-    func simulatePullToRefresh(){
-        allTargets.forEach({ target in
-            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach({
-                (target as NSObject).perform(Selector($0))
-            })
-        })
-    }
-}
+
 
 private extension UIButton {
     func simulateTap() {
@@ -527,20 +519,5 @@ private extension UIButton {
     }
 }
 
-
-
-private class FakeRefreshControl:UIRefreshControl{
-    private var _isRefreshing = false
-    override var isRefreshing: Bool {
-        return _isRefreshing
-    }
-    override func beginRefreshing() {
-        _isRefreshing = true
-    }
-    
-    override func endRefreshing() {
-        _isRefreshing = false
-    }
-}
 
 
---
+++ EssentialFeediOSTests/UIRefreshControl+TestHelpers.swift
@@ -0,0 +1,33 @@
+//
+//  UIRefreshControl+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by macbook abdul on 27/05/2024.
+//
+
+import Foundation
+import UIKit
+
+ class FakeRefreshControl:UIRefreshControl{
+    private var _isRefreshing = false
+    override var isRefreshing: Bool {
+        return _isRefreshing
+    }
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+}
+
+ extension UIRefreshControl {
+    func simulatePullToRefresh(){
+        allTargets.forEach({ target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach({
+                (target as NSObject).perform(Selector($0))
+            })
+        })
+    }
+}

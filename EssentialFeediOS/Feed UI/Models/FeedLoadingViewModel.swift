825f34672a38381732bb5aa8f8df2c893a043022
abdul ahad <abdul_ahad1996@live.com>
2024-06-04 21:15:57 +0200

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/Models/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by macbook abdul on 04/06/2024.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading:Bool
+}
---
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by macbook abdul on 04/06/2024.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed:[FeedImage]
+}

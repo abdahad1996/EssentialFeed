4dcf9a45f5dda7152d1ccaa434469f41171a17eb
abdul ahad <abdul_ahad1996@live.com>
2024-06-06 14:40:33 +0200

    Display image with animation

--- EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -37,7 +37,7 @@ final class FeedImageCellController:FeedImageView {
         cell?.locationContainer.isHidden = !viewModel.hasLocation
         cell?.locationLabel.text = viewModel.location
         cell?.descriptionLabel.text = viewModel.description
-        
+        cell?.feedImageView.setImageAnimated(viewModel.image)
         cell?.onRetry = delegate.didRequestImage
         
         cell?.feedImageContainer.isShimmering = viewModel.isLoading

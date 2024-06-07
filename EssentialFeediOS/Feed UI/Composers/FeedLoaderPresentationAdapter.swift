fe00ff1f85400ed4daeeb07f7ae4955646a8dbc4
abdul ahad <abdul_ahad1996@live.com>
2024-06-07 22:22:18 +0200

    Move FeedLoaderPresentationAdapter to separate file

---
+++ EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,35 @@
+//
+//  FeedImageDataLoaderPresentationAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by macbook abdul on 07/06/2024.
+//
+
+import Foundation
+import EssentialFeed
+import UIKit
+
+class FeedLoaderPresentationAdapter:FeedViewControllerDelegate {
+    
+    var presenter:FeedPresenter?
+    let loader:FeedLoader
+    
+    init( loader: FeedLoader) {
+       
+        self.loader = loader
+    }
+    
+    func didRequestFeedRefresh(){
+        presenter?.didStartLoadingFeed()
+        
+        loader.load {[weak self] result in
+            switch result {
+            case .success(let feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+            
+        }
+    }
+}
--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -40,30 +40,6 @@ private extension FeedViewController {
         return feedController
     }
 }
-class FeedLoaderPresentationAdapter:FeedViewControllerDelegate {
-    
-    var presenter:FeedPresenter?
-    let loader:FeedLoader
-    
-    init( loader: FeedLoader) {
-       
-        self.loader = loader
-    }
-    
-    func didRequestFeedRefresh(){
-        presenter?.didStartLoadingFeed()
-        
-        loader.load {[weak self] result in
-            switch result {
-            case .success(let feed):
-                self?.presenter?.didFinishLoadingFeed(with: feed)
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-            
-        }
-    }
-}
 
 
 

a4eb88bc81ac2bfafc84dffa625c9535e36116db
abdul ahad <abdul_ahad1996@live.com>
2024-06-10 12:59:05 +0200

    add test for loading method

--- EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
+++ EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -8,12 +8,25 @@
 import Foundation
 import XCTest
 
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedLoadingView{
+    func display(_ viewModel:FeedLoadingViewModel)
+}
+
 class FeedPresenter {
     
-    init(view:Any){
-        
+    let feedLoadingView:FeedLoadingView
+    
+    init(feedLoadingView:FeedLoadingView){
+        self.feedLoadingView = feedLoadingView
     }
     
+    func didStartLoadingFeed(){
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
 }
 class FeedPresenterTests: XCTestCase {
     
@@ -25,18 +38,37 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        
+        let (sut, view) = makeSUT()
+
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages,[.feedLoading(isLoading: true)])
+        
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(feedLoadingView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy:FeedLoadingView {
+        
+        enum Message:Equatable {
+            case feedLoading(isLoading: Bool)
+
+            
+        }
+        var messages = [Message]()
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.feedLoading(isLoading: viewModel.isLoading))
+        }
     }
     
 }

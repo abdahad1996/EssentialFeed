9329049ad5ab0d60cbdebab91f9eac9a0f5335c8
abdul ahad <abdul_ahad1996@live.com>
2024-06-14 22:43:16 +0200

    CoreDataFeedStore.retrieveImageData delivers image data not found… … when store is not empty but there's no image with matching URL

--- EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/FeedCache/CoreDataFeedImageDataStoreTests.swift
@@ -28,6 +28,18 @@ class CoreDataFeedImageDataStoreTests:XCTestCase{
             expect(sut, toCompleteRetrievalWith: notFound(), for: anyUrl())
         }
     
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() {
+        let sut = makeSUT()
+        let url = URL(string: "http://a-url.com")!
+
+        
+        let nonMatchingURL = URL(string: "http://another-url.com")!
+        insert(anyData(), for: url, into: sut)
+        expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
+
+
+    }
+    
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
             let storeBundle = Bundle(for: CoreDataFeedStore.self)
             let storeURL = URL(fileURLWithPath: "/dev/null")
@@ -55,4 +67,27 @@ class CoreDataFeedImageDataStoreTests:XCTestCase{
             wait(for: [exp], timeout: 1.0)
         }
     
+    private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #file, line: UInt = #line) {
+            let exp = expectation(description: "Wait for cache insertion")
+        let image = localImage(url: url)
+            sut.insert([image], timestamp: Date()) { result in
+                switch result {
+                case let .failure(error):
+                    XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+
+                case .success:
+                    sut.insert(data, for: url) { result in
+                        if case let Result.failure(error) = result {
+                            XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
+                        }
+                    }
+                }
+                exp.fulfill()
+            }
+            wait(for: [exp], timeout: 1.0)
+        }
+    
+    private func localImage(url: URL) -> LocalFeedImage {
+        return LocalFeedImage(id: UUID(), description: "any", location: "any", imageURL: url)
+        }
 }

204c096e005c70e4fac9cc67dad93f4e06aa1b72
abdul ahad <abdul_ahad1996@live.com>
2024-06-15 18:08:23 +0200

    LocalFeedLoader in integration with the CoreDataFeedStore does no… …t delete recently saved items saved by separate instances when validating cache, proving we correctly manage the data models on disk.

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -78,6 +78,19 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
             expect(imageLoaderToPerformLoad, toLoad: lastImageData, for: image.url)
         }
 
+    func test_validateFeedCache_doesNotDeleteRecentlySavedFeed() {
+        
+        let feedLoaderToPerformSave = makeFeedLoader()
+        let feedLoaderToPerformValidation = makeFeedLoader()
+        
+        let feed = uniqueImages().models
+        save(feed, with: feedLoaderToPerformSave)
+
+        validateCache(with: feedLoaderToPerformValidation)
+        expect(feedLoaderToPerformSave, toLoad: feed)
+
+    }
+    
     // MARK: Helpers
     
     private func makeFeedLoader(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
@@ -149,6 +162,17 @@ class EssentialFeedCacheIntegrationTests: XCTestCase {
                 exp.fulfill()
             }
             wait(for: [exp], timeout: 1.0)
+        }
+    
+    private func validateCache(with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
+            let saveExp = expectation(description: "Wait for save completion")
+            loader.validateCache() { result in
+                if case let Result.failure(error) = result {
+                    XCTFail("Expected to validate feed successfully, got error: \(error)", file: file, line: line)
+                }
+                saveExp.fulfill()
+            }
+            wait(for: [saveExp], timeout: 1.0)
         }
 
     private func setupEmptyStoreState() {

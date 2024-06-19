4cc256f2dd92bd81ab74eeffe629a67a5bfef523
abdul ahad <abdul_ahad1996@live.com>
2024-06-20 01:43:07 +0200

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentailApp/EssentailAppUITests/EssentailAppUITests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentailAppUITests.swift
+//  EssentailAppUITests
+//
+//  Created by macbook abdul on 20/06/2024.
+//
+
+import XCTest
+
+final class EssentailAppUITests: XCTestCase {
+
+    
+}

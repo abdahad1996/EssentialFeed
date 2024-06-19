d92a95283cc19f5c28e082976eb3a8252143d6de
abdul ahad <abdul_ahad1996@live.com>
2024-06-20 01:53:32 +0200

    Displays remote feed on launch when customer has connectivity

--- EssentailApp/EssentailAppUITests/EssentailAppUITests.swift
+++ EssentailApp/EssentailAppUITests/EssentailAppUITests.swift
@@ -9,5 +9,15 @@ import XCTest
 
 final class EssentailAppUITests: XCTestCase {
 
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+        
+    }
     
 }

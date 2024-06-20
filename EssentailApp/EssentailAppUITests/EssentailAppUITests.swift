7cd84b9c98eddce867f0e8a43bce2f426c9c70d8
abdul ahad <abdul_ahad1996@live.com>
2024-06-20 02:08:27 +0200

    Displays cached feed on launch when customer has no connectivity

--- EssentailApp/EssentailAppUITests/EssentailAppUITests.swift
+++ EssentailApp/EssentailAppUITests/EssentailAppUITests.swift
@@ -24,4 +24,19 @@ final class EssentailAppUITests: XCTestCase {
         
     }
     
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
+            let onlineApp = XCUIApplication()
+            onlineApp.launch()
+
+            let offlineApp = XCUIApplication()
+            offlineApp.launchArguments = ["-connectivity", "offline"]
+            offlineApp.launch()
+
+            let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+            XCTAssertEqual(cachedFeedCells.count, 22)
+
+            let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+            XCTAssertTrue(firstCachedImage.exists)
+        }
+    
 }

87b56121d9b7a5ba33af745b4be1a49546aa5806
abdul ahad <abdul_ahad1996@live.com>
2024-06-23 13:36:43 +0200

    Add ImageComment data model

--- EssentialFeed/EssentialFeed/Feed Feature/ImageComment.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/ImageComment.swift
@@ -7,18 +7,18 @@
 
 import Foundation
 
-//public struct ImageComment:Hashable {
-//    
-//    public let id: UUID
-//    public let description: String?
-//    public let location: String?
-//    public let url: URL
-//    
-//    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
-//        self.id = id
-//        self.description = description
-//        self.location = location
-//        self.url = imageURL
-//    }
-//}
+public struct ImageComment:Hashable {
+    
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = imageURL
+    }
+}
 

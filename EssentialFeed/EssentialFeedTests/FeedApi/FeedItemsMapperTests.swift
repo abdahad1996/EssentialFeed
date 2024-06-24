023b74db418df47cb92f8eac8fc2e80f352074bd
abdul ahad <abdul_ahad1996@live.com>
2024-06-24 22:08:42 +0200

    Move test helpers to shared scope

--- EssentialFeed/EssentialFeedTests/FeedApi/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/FeedItemsMapperTests.swift
@@ -87,17 +87,9 @@ class FeedItemsMapperTests:XCTest{
         
     }
     
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
+    
     
     
 }
 
 
-private extension HTTPURLResponse {
-    convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-    }
-}

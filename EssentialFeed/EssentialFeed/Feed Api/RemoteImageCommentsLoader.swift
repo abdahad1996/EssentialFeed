a2dc885dcfb2b6ba717631ef125186a72edd2eaf
abdul ahad <abdul_ahad1996@live.com>
2024-06-24 18:38:56 +0200

    Replace RemoteImageCommentsLoader with generic RemoteLoader to remote… … duplication

--- EssentialFeed/EssentialFeed/Feed Api/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteImageCommentsLoader.swift
@@ -7,48 +7,11 @@
 
 import Foundation
 
-public class RemoteImageCommentsLoader{
-    
-    private let url:URL
-    private let client:HTTPClient
-    
-    public enum Error:Swift.Error{
-        case connectivity
-        case invalidData
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentsLoader {
+    convenience init(url: URL, client: HTTPClient)  {
+        self.init(url: url, client: client, mapper: ImageCommentsMapper.map)
     }
-    
-    public typealias Result = Swift.Result<[ImageComment],Error>
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-   
-    
-    public func load(completion:@escaping (Result) -> Void){
-    
-        client.get(from: url, completion: {[weak self] result in
-            guard self != nil else{return}
-            switch result {
-            case .success((let data,let response)):
-                completion(RemoteImageCommentsLoader.map(data, response))
-             
-            case .failure(_):
-                completion(.failure(RemoteImageCommentsLoader.Error.connectivity))
-            }
-            
-        })
-    }
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
-        do {
-            let items =  try ImageCommentsMapper.map(data, response)
-            return .success(items)
-        }
-        catch (_) {
-          return .failure(RemoteImageCommentsLoader.Error.invalidData)
-        }
-     }
-   
 }
 

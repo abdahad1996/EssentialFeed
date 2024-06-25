b1f45e7f943afea3b2f27ec79a6e26e67f2f6933
abdul ahad <abdul_ahad1996@live.com>
2024-06-25 04:29:40 +0200

    Remove unused test spy

--- EssentialFeed/EssentialFeedTests/Shared Api/Helpers/HTTPClientSpy.swift
+++ EssentialFeed/EssentialFeedTests/Shared Api/Helpers/HTTPClientSpy.swift
@@ -8,41 +8,41 @@
 import Foundation
 import EssentialFeed
 
-class HTTPClientSpy:HTTPClient {
-    
-    private struct Task:HTTPClientTask{
-        let callBack:() -> Void
-        func cancel(){
-            callBack()
-        }
-    }
-   private var completions = [(url:URL,completion:(HTTPClient.Result) -> Void)]()
-    
-    var requestedUrls:[URL] {
-        completions.map {$0.url}
-    }
-    
-    var cancelledURLs = [URL]()
-    
-    @discardableResult
-    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completions.append((url:url,completion:completion))
-        return Task(callBack: { [weak self] in
-            self?.cancelledURLs.append(url)
-        })
-    }
-    
-    func complete(with error:Error,at index:Int = 0) {
-        completions[index].completion(.failure(error))
-    }
-    
-    func complete(with data:Data,statusCode:Int,at index:Int = 0) {
-        let response = HTTPURLResponse(url: requestedUrls[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)!
-        completions[index].completion(.success((data, response)))
-    }
-    
-}
-
-
-
-
+//class HTTPClientSpy:HTTPClient {
+//    
+//    private struct Task:HTTPClientTask{
+//        let callBack:() -> Void
+//        func cancel(){
+//            callBack()
+//        }
+//    }
+//   private var completions = [(url:URL,completion:(HTTPClient.Result) -> Void)]()
+//    
+//    var requestedUrls:[URL] {
+//        completions.map {$0.url}
+//    }
+//    
+//    var cancelledURLs = [URL]()
+//    
+//    @discardableResult
+//    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+//        completions.append((url:url,completion:completion))
+//        return Task(callBack: { [weak self] in
+//            self?.cancelledURLs.append(url)
+//        })
+//    }
+//    
+//    func complete(with error:Error,at index:Int = 0) {
+//        completions[index].completion(.failure(error))
+//    }
+//    
+//    func complete(with data:Data,statusCode:Int,at index:Int = 0) {
+//        let response = HTTPURLResponse(url: requestedUrls[index], statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+//        completions[index].completion(.success((data, response)))
+//    }
+//    
+//}
+//
+//
+//
+//

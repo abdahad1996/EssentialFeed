75f00cd0f4739cd383a075628c52bf80324f6424
abdul ahad <abdul_ahad1996@live.com>
2024-06-26 19:24:06 +0200

    Add image comments title

---
+++ EssentialFeed/EssentialFeed/Image Comment Presentation/ImageCommentPresenter.swift
@@ -0,0 +1,25 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeed
+//
+//  Created by macbook abdul on 10/06/2024.
+//
+
+import Foundation
+public struct ImageCommentViewModel {
+    public let feed: [ImageComment]
+}
+final public class ImageCommentPresenter {
+    
+    
+    public static var title: String {
+            NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+                tableName: "ImageComments",
+                bundle: Bundle(for: Self.self),
+                comment: "Title for the image comments view")
+        }
+    
+//    public static func map(_ imageComment:[ImageComment]) -> ImageCommentViewModel{
+//        ImageCommentViewModel.init(feed: imageComment)
+//    }
+}
---
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -0,0 +1,47 @@
+//
+//  ImageCommentsPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by macbook abdul on 26/06/2024.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+class ImageCommentPresenterTests: XCTestCase {
+    
+    func test_title_isLocalized() {
+            XCTAssertEqual(ImageCommentPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+        }
+    
+//    func test_map_createsViewModel() {
+//        let feed = uniqueImages().models
+//
+//        let viewModel = ImageCommentPresenter.map(feed)
+//        
+//        XCTAssertEqual(viewModel.feed, feed)
+//        
+//        
+//    }
+    
+
+    
+    // MARK: - Helpers
+    
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table:String =  "ImageComments"
+        let bundle = Bundle(for: ImageCommentPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+    
+
+}
+
+
+
+

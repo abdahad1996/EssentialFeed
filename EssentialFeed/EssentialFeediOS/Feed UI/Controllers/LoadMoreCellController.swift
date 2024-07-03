a8b3989ba95def53544dec18bdaf1147c6f850c9
abdul ahad <abdul_ahad1996@live.com>
2024-07-03 16:25:56 +0200

    Remove cell selection style

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -22,7 +22,8 @@ public class LoadMoreCellController:NSObject,UITableViewDataSource,UITableViewDe
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cell
+        cell.selectionStyle = .none
+        return cell
     }
     
     

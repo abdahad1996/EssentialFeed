be36d26b4fce0568f19e195aa7a1c2d0b90b8890
abdul ahad <abdul_ahad1996@live.com>
2024-07-03 13:44:48 +0200

    Add "Load more" message label

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -23,11 +23,13 @@ public class LoadMoreCellController:NSObject,UITableViewDataSource{
     
 }
 
-extension LoadMoreCellController: ResourceLoadingView {
+extension LoadMoreCellController: ResourceLoadingView,ResourceErrorView {
+    public func display(_ viewModel: ResourceErrorViewModel) {
+        cell.message = viewModel.message
+    }
+    
     public func display(_ viewModel: ResourceLoadingViewModel) {
         cell.isLoading = viewModel.isLoading
     }
-    
-    
     
 }

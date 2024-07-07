7a2c5ebc7ed3cb5daaa4cc5eefe785bfad7c9878
abdul ahad <abdul_ahad1996@live.com>
2024-07-08 01:27:22 +0200

    Promote InMemoryFeedStore to production Replaced Dictionary with NSCache to support automatic eviction when resources are low.

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/InMemoryFeedStore.swift
@@ -0,0 +1,39 @@
+//
+//  InMemoryFeedStore.swift
+//  EssentialFeed
+//
+//  Created by macbook abdul on 08/07/2024.
+//
+
+import Foundation
+
+public class InMemoryFeedStore {
+    private var feedCache: CachedFeed?
+    private var feedImageDataCache = NSCache<NSURL, NSData>()
+
+    public init() {}
+}
+
+extension InMemoryFeedStore: FeedStore {
+    public func deleteCachedFeed() throws {
+        feedCache = nil
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
+        feedCache = CachedFeed(feed: feed, timestamp: timestamp)
+    }
+
+    public func retrieve() throws -> CachedFeed? {
+        feedCache
+    }
+}
+
+extension InMemoryFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL) throws {
+        feedImageDataCache.setObject(data as NSData, forKey: url as NSURL)
+    }
+
+    public func retrieve(dataForURL url: URL) throws -> Data? {
+        feedImageDataCache.object(forKey: url as NSURL) as Data?
+    }
+}
---
+++ EssentialFeed/EssentialFeedTests/FeedCache/InMemoryFeedImageDataStoreTests.swift
@@ -0,0 +1,46 @@
+//
+//  InMemoryFeedImageDataStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by macbook abdul on 08/07/2024.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+class InMemoryFeedImageDataStoreTests: XCTestCase, FeedImageDataStoreSpecs {
+
+    func test_retrieveImageData_deliversNotFoundWhenEmpty() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversNotFoundOnEmptyCache(on: sut)
+    }
+
+    func test_retrieveImageData_deliversNotFoundWhenStoredDataURLDoesNotMatch() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversNotFoundWhenStoredDataURLDoesNotMatch(on: sut)
+    }
+
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversFoundDataWhenThereIsAStoredImageDataMatchingURL(on: sut)
+    }
+
+    func test_retrieveImageData_deliversLastInsertedValue() throws {
+        let sut = makeSUT()
+
+        assertThatRetrieveImageDataDeliversLastInsertedValueForURL(on: sut)
+    }
+
+    // - MARK: Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> InMemoryFeedStore {
+        let sut = InMemoryFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+}

2062746abfd7dc8fe0ebfa1edefa9314ffbad675
abdul ahad <abdul_ahad1996@live.com>
2024-07-07 20:59:53 +0200

    Make NullStore FeedStore implementation sync

--- EssentailApp/EssentailApp/Composers/NullStore.swift
+++ EssentailApp/EssentailApp/Composers/NullStore.swift
@@ -9,18 +9,22 @@ import Foundation
 import EssentialFeed
 
 class NullStore: FeedStore {
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(.success(()))
-    }
-
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        completion(.success(()))
-    }
-
-    func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.success(.none))
-    }
-
+//    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+//        completion(.success(()))
+//    }
+//
+//    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+//        completion(.success(()))
+//    }
+//
+//    func retrieve(completion: @escaping RetrievalCompletion) {
+//        completion(.success(.none))
+//    }
+    
+    func deleteCachedFeed() throws {}
+    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {}
+    func retrieve() throws -> CachedFeed? {return .none}
+    
 }
 extension NullStore:FeedImageDataStore{
     func insert(_ data: Data, for url: URL) throws {}
